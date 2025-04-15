dca3764f7e2fb9fb7e6e87ec24b891dc217fa77e
Peter Columbus <peter.johannessen@columbusglobal.com>
2025-04-15 11:10:18 +0200

    1

--- gitstars.py
+++ gitstars.py
@@ -1,6 +1,9 @@
 import httpx
 
+from prefect import flow, task # Prefect flow and task decorators
 
+
+@flow(log_prints=True)
 def show_stars(github_repos: list[str]):
     """Show the number of stars that GitHub repos have"""
     for repo in github_repos:
@@ -8,17 +11,17 @@ def show_stars(github_repos: list[str]):
         stars = get_stars(repo_stats)
         print(f"{repo}: {stars} stars")
 
-
+@task
 def fetch_stats(github_repo: str):
     """Fetch the statistics for a GitHub repo"""
     return httpx.get(f"https://api.github.com/repos/{github_repo}").json()
 
-
+@task
 def get_stars(repo_stats: dict):
     """Get the number of stars from GitHub repo statistics"""
     return repo_stats['stargazers_count']
 
-
+# Run the flow if this script is executed directly
 if __name__ == "__main__":
     show_stars([
         "PrefectHQ/prefect",
