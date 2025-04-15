7554180aba444661c33341f827c39d2b81004ae1
Peter Columbus <dev.pejo422@outlook.com>
2025-04-15 15:53:43 +0200

    hy

--- deploy_jokes.py
+++ deploy_jokes.py
@@ -1,8 +1,20 @@
 from prefect import flow
+from prefect.deployments import Deployment
+from prefect.server.schemas.schedules import CronSchedule
+from prefect.filesystems import GitHub
+
+from daily_joke import upload_joke_to_db_flow
 
 if __name__ == "__main__":
-    flow.daily_joke.deploy(
+    Deployment.build_from_flow(
+        flow=upload_joke_to_db_flow,
         name="daily-joke-deployment",
         work_pool_name="my-work-pool",
-        cron="0 * * * *",  # Run every day at midnight
-    )
+        schedule=CronSchedule(cron="0 * * * *", timezone="UTC"),  # Hourly
+        path=".",
+        storage=GitHub(
+            repo="PeJo422/prefect",
+            reference="main",  # or "master" if you're living in the 1900s
+            access_token="{{ secrets.GITHUB_TOKEN }}"  # set this in your Prefect Cloud secrets
+        ),
+    ).apply()
